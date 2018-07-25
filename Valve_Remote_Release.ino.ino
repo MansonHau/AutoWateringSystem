0098af48bf0631873eae8b2f1618b81720b36178
MansonHau <34991227+MansonHau@users.noreply.github.com>
2018-07-24 22:28:34 -0400

    Add files via upload
    Code for ESP8266 based on aREST platform. Currently 1 control element.

---
+++ Valve_Remote_Release.ino.ino
@@ -0,0 +1,67 @@
+/*
+  Remote Valve Release - For Watering System
+*/
+
+// Import required libraries
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <aREST.h>
+
+// Clients
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Create aREST instance
+aREST rest = aREST(client);
+
+// aREST API key (that you can get at dashboard.arest.io)
+char * key = "ha3o84eg4358w31e";
+
+// WiFi parameters
+const char* ssid = "MJMH";
+const char* password = "4166892113";
+
+// Functions
+void callback(char* topic, byte* payload, unsigned int length);
+
+void setup(void)
+{
+  // Start Serial
+  Serial.begin(115200);
+
+  // Set aREST API key
+  rest.setKey(key);
+
+  // Set callback
+  client.setCallback(callback);
+
+  // Give name to device
+  rest.set_name("ValveRelease");
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Set output topic
+  char* out_topic = rest.get_topic();
+
+}
+
+void loop() {
+
+  // Connect to the cloud
+  rest.handle(client);
+
+}
+
+// Handles message arrived on subscribed topic(s)
+void callback(char* topic, byte* payload, unsigned int length) {
+
+  rest.handle_callback(client, topic, payload, length);
+
+}
